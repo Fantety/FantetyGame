766ee6d782439b876435995e133a412a1ca7ded4
Fantety <fantety@outlook.com>
2022-11-18 14:33:33 +0800

    feat : balcony plot

--- src/DialogScript.gd
+++ src/DialogScript.gd
@@ -1,3 +1,12 @@
 extends Node2D
 
 var isSearchComputer = false
+
+
+#balcony plot
+var balconyFirstChoise = 0
+var balconySecondChoise = 0
+
+func balcony_finish() -> bool:
+	return balconyFirstChoise==2 and balconySecondChoise == 2
+	
