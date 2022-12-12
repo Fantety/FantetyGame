f53749c2f5faf859e6c533faffc1a662e1657ff4
Fantety <fantety@outlook.com>
2022-12-12 14:51:29 +0800

    feat : ui style

--- src/furniture/DoorCtrlUi.gd
+++ src/furniture/DoorCtrlUi.gd
@@ -4,6 +4,11 @@ func _ready():
 	set_visible(false)
 	pass
 
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel") && visible == true:
+		door_ctrl_ui_hide()
+	pass
+
 signal door_ctrl_ui_input_finished(parentName:String, passward)
 func _on_line_edit_door_ctrl_ok_button_pressed():
 	emit_signal("door_ctrl_ui_input_finished",get_parent().name, $Panel/VBoxContainer/LineEdit.get_text())
